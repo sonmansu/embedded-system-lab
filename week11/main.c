33b7f73ee887245af0b4e1c425daa3b7432a2b85
jung980514 <64721658+jung980514@users.noreply.github.com>
2021-11-17 16:23:50 +0900

    Update main.c

--- week11/main.c
+++ week11/main.c
@@ -3,7 +3,6 @@
 #include "stm32f10x_gpio.h"
 #include "stm32f10x_usart.h"
 #include "stm32f10x_rcc.h"
-#include "stm32f10x_tim.h"
 #include "lcd.h"
 #include "touch.h"
 #include "misc.h"
@@ -31,132 +30,162 @@ void RCC_Configure(void)
       RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE); // port C RCC ENABLE
       /* PWM */
       RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE); // TIM2
+      RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE); // Port B
+      RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE); // TIM3
       /* LED */
       RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE); // Port D
+      RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
 
-   RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
 }
 
 void GPIO_Configure(void)
 {
+
     GPIO_InitTypeDef GPIO_InitStructure3;
-    GPIO_InitStructure3.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3; // led1,2
+    GPIO_InitStructure3.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
     GPIO_InitStructure3.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_InitStructure3.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_Init(GPIOD, &GPIO_InitStructure3); // led È°¼ºÈ­
+    GPIO_Init(GPIOD, &GPIO_InitStructure3); // led í™œì„±í™”
+
+   /* PWM */
+    GPIO_InitTypeDef GPIO_InitStructure2;
+    GPIO_InitStructure2.GPIO_Pin = GPIO_Pin_0;
+    GPIO_InitStructure2.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure2.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_Init(GPIOB, &GPIO_InitStructure2);
+
 }
 
 void NVIC_Configure(void) {
+    
+     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);
+     NVIC_InitTypeDef NVIC_InitStructure;
+     /* Enable TIM2 Global Interrupt */
+     NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+     NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+     NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+     NVIC_Init(&NVIC_InitStructure);
 
-   NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);
-   NVIC_InitTypeDef NVIC_InitStructure;
-
-   /* Enable TIM2 Global Interrupt */
-   NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
-   NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-   NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-   NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-   NVIC_Init(&NVIC_InitStructure);
 }
+
 
 void TIM2_IRQHandler(void) {
-  if(TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET)
-  {
-      if (ledCount % 2 == 0) { //led 1(pin2) ´Â 1ÃÊ¸¶´Ù
-      GPIO_SetBits(GPIOD, GPIO_Pin_2);
-      } else{
-      GPIO_ResetBits(GPIOD, GPIO_Pin_2);
+      if(TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET)
+      {
+          if (ledCount % 2 == 0) {
+          GPIO_SetBits(GPIOD, GPIO_Pin_2);
+          } else{
+          GPIO_ResetBits(GPIOD, GPIO_Pin_2); 
+          }
+
+          if(ledCount == 0) {
+          GPIO_ResetBits(GPIOD, GPIO_Pin_3);
+          } else if(ledCount == 5) {
+          GPIO_SetBits(GPIOD, GPIO_Pin_3);
+          }
+
+          ledCount++;
+          ledCount %= 10;
+
+          TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
       }
-
-      if(ledCount == 0) { //led2(pin3)Àº 5ÃÊ¸¶´Ù
-      GPIO_ResetBits(GPIOD, GPIO_Pin_3);
-      } else if(ledCount == 5) {
-      GPIO_SetBits(GPIOD, GPIO_Pin_3);
-      }
-
-      ledCount++;
-      ledCount %= 10;
-
-      TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
-  }
 }
 
-void PWM_Configure()
+void PWM_Configure1()
 {
-prescale = (uint16_t) (SystemCoreClock / 1000000);
-TIM_TimeBaseStructure.TIM_Period = 20000;
-TIM_TimeBaseStructure.TIM_Prescaler = prescale;
-TIM_TimeBaseStructure.TIM_ClockDivision = 0;
-TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Down;
-TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
-TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
-TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
-TIM_OCInitStructure.TIM_Pulse = 1500; // us
-TIM_OC3Init(TIM3, &TIM_OCInitStructure);
-TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
-TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Disable);
-TIM_ARRPreloadConfig(TIM3, ENABLE);
-TIM_Cmd(TIM3, ENABLE);
+     prescale = (uint16_t) (SystemCoreClock / 10000);
+     TIM_TimeBaseStructure.TIM_Period = 10000;
+     TIM_TimeBaseStructure.TIM_Prescaler = prescale;
+     TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+     TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Down;
+     TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+     TIM_ARRPreloadConfig(TIM2, ENABLE);
+     TIM_Cmd(TIM2, ENABLE);
 }
 
-//void PWM_Configure()
-//{
-//   prescale = (uint16_t) (SystemCoreClock / 10000);
-//   TIM_TimeBaseStructure.TIM_Period = 10000;
-//   TIM_TimeBaseStructure.TIM_Prescaler = prescale;
-//   TIM_TimeBaseStructure.TIM_ClockDivision = 0;
-//   TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Down;
-//
-//   TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
-//   TIM_ARRPreloadConfig(TIM2, ENABLE);
-//   TIM_Cmd(TIM2, ENABLE);
-//}
+void PWM_Configure2()
+{
+     prescale = (uint16_t) (SystemCoreClock / 1000000);
+     TIM_TimeBaseStructure.TIM_Period = 20000;
+     TIM_TimeBaseStructure.TIM_Prescaler = prescale;
+     TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+     TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Down;
+     TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+     TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+     TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+     TIM_OCInitStructure.TIM_Pulse = 1500; // us
+     TIM_OC3Init(TIM3, &TIM_OCInitStructure);
+     TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
+     TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Disable);
+     TIM_ARRPreloadConfig(TIM3, ENABLE);
+     TIM_Cmd(TIM3, ENABLE);
+}
+void delay()
+{
+     for(int j = 0; j < 5000000; j++) {
+        continue;
+     }
+}
 
 
 int main(void)
 {
 
-    SystemInit();
-    RCC_Configure();
-    GPIO_Configure();
-    PWM_Configure();
-    NVIC_Configure();
+      SystemInit();
+      RCC_Configure();
+      GPIO_Configure();
+      PWM_Configure1();
+      PWM_Configure2();
 
-    //-----------------
+      NVIC_Configure();
 
-    LCD_Init(); // LCD ÃÊ±âÈ­
-    Touch_Configuration(); // ÅÍÄ¡ ¼³Á¤
-    Touch_Adjust(); // È­¸é ÅÍÄ¡ ÃÊÁ¡ ¸ÂÃß±â
-    LCD_Clear(WHITE); // LCD ¹è°æ ÃÊ±âÈ­
+      //-----------------
 
-    //-----------------
+      LCD_Init(); // LCD ì´ˆê¸°í™”
+      Touch_Configuration(); // í„°ì¹˜ ì„¤ì •
+      Touch_Adjust(); // í™”ë©´ í„°ì¹˜ ì´ˆì  ë§žì¶”ê¸°
+      LCD_Clear(WHITE); // LCD ë°°ê²½ ì´ˆê¸°í™”
 
-    uint16_t pos_temp[2]; // x, yÁÂÇ¥¸¦ ´ãÀ» ¹è¿­
+      //-----------------
+      uint16_t rotation[2] = {700, 2300};
+      int i = 0;
 
-   LCD_ShowString(40, 10, "THU_TEAM07", MAGENTA, WHITE); // ÆÀ¸í Ãâ·Â
-   LCD_ShowString(90, 50, "OFF", RED, WHITE); // µðÆúÆ®·Î OFF
-   LCD_DrawRectangle(40, 80, 80, 120); // »ç°¢Çü Ãâ·Â
-   LCD_ShowString(60,100, "BTN", MAGENTA, WHITE); // "Button" ±ÛÀÚ Ãâ·Â
+      uint16_t pos_temp[2]; // x, yì¢Œí‘œë¥¼ ë‹´ì„ ë°°ì—´
 
-    while (1) {
-         Touch_GetXY(&pos_temp[0], &pos_temp[1], 1); // ÅÍÄ¡ ÁÂÇ¥ ¹Þ¾Æ¼­ ¹è¿­¿¡ ÀÔ·Â
-         Convert_Pos(pos_temp[0],pos_temp[1],&pos_temp[0],&pos_temp[1]); // ¹ÞÀº ÁÂÇ¥¸¦ LCD Å©±â¿¡ ¸Â°Ô º¯È¯
-         printf("%d, %d\n", pos_temp[0], pos_temp[1]);
+       LCD_ShowString(40, 10, "THU_Team07", MAGENTA, WHITE); // íŒ€ëª… ì¶œë ¥
+       LCD_ShowString(90, 50, "OFF", RED, WHITE); // ë””í´íŠ¸ë¡œ OFF 
+       LCD_DrawRectangle(40, 80, 80, 120); // ì‚¬ê°í˜• ì¶œë ¥
+       LCD_ShowString(50,90, "BTN", MAGENTA, WHITE); // "Button" ê¸€ìž ì¶œë ¥
+      
+      while (1) {
+           Touch_GetXY(&pos_temp[0], &pos_temp[1], 1); // í„°ì¹˜ ì¢Œí‘œ ë°›ì•„ì„œ ë°°ì—´ì— ìž…ë ¥
+           Convert_Pos(pos_temp[0],pos_temp[1],&pos_temp[0],&pos_temp[1]); // ë°›ì€ ì¢Œí‘œë¥¼ LCD í¬ê¸°ì— ë§žê²Œ ë³€í™˜
 
-         if ((uint16_t)40 < pos_temp[0] && pos_temp[0] < (uint16_t)80 && \
-             (uint16_t)80 < pos_temp[1] && pos_temp[1] < (uint16_t)120) {
-            if (flag) {
-               LCD_ShowString(90, 50, "    ", RED, WHITE);
-               LCD_ShowString(40, 50, "ON", RED, WHITE);
-               TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE); // interrupt enable
-            } else {
-               LCD_ShowString(90, 50, "OFF", RED, WHITE);
-               LCD_ShowString(40, 50, "    ", RED, WHITE);
-               TIM_ITConfig(TIM2, TIM_IT_Update, DISABLE); // interrupt enable
-            }
-            flag++;
-            flag %= 2;
-         }
-    }
-    return 0;
+           if ((uint16_t)40 < pos_temp[0] && pos_temp[0] < (uint16_t)80 && \
+               (uint16_t)80 < pos_temp[1] && pos_temp[1] < (uint16_t)120) {
+              if (flag) {
+                 LCD_ShowString(90, 50, "    ", RED, WHITE); 
+                 LCD_ShowString(40, 50, "ON", RED, WHITE); 
+                 TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE); // interrupt enable
+              } else {
+                 LCD_ShowString(90, 50, "OFF", RED, WHITE); 
+                 LCD_ShowString(40, 50, "    ", RED, WHITE); 
+                 TIM_ITConfig(TIM2, TIM_IT_Update, DISABLE); // interrupt enable
+              }
+              flag++;
+              flag %= 2;
+           }
+
+           TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+           TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+           TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+           TIM_OCInitStructure.TIM_Pulse = rotation[i]; // us
+           TIM_OC3Init(TIM3, &TIM_OCInitStructure);
+            i++;
+            i %= 2;
+            delay();
+
+      }
+      return 0;
 }
