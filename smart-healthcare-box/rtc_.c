3ef36e5363ff9dac1c569c68588141ee181e5543
ambivalent46 <62247273+ambivalent46@users.noreply.github.com>
2021-12-02 14:43:39 +0900

    Update rtc_.c

--- smart-healthcare-box/rtc_.c
+++ smart-healthcare-box/rtc_.c
@@ -1,6 +1,9 @@
 // http://poohyhoh.blogspot.com/2010/09/arm-cortex-m3-stm32-power-control-pwr-5.html
 // ...
+
+#include "stm32f10x_bkp.h" //?
 #include "stm32f10x_rtc.h"
+
 #define CRL_CNF_Set      ((uint16_t)0x0010)      /*!< Configuration Flag Enable Mask */
 #define CRL_CNF_Reset    ((uint16_t)0xFFEF)      /*!< Configuration Flag Disable Mask */
 #define PRLH_MSB_Mask    ((uint32_t)0x000F0000)  /*!< RTC Prescaler MSB Mask */
@@ -32,8 +35,27 @@ void RTC_IRQHandler(void) {
 }
 
 void RTC_Configuration(void) {
-    /*
-    */
+   
+   /* RTC clock source configuration */ /* Allow access to BKP Domain */
+    PWR_BackupAccessCmd(ENABLE);
+    
+    BKP_DeInit(); /* Reset Backup Domain */
+
+    RCC_LSICmd(ENABLE);
+    while(RCC_GetFlagStatus(RCC_FLAG_LSIRDY) == RESET) { }
+
+    RCC_RTCCLKConfig(RCC_RTCCLKSource_LSI); /* Select the RTC Clock Source */
+    RCC_RTCCLKCmd(ENABLE); /* Enable the RTC Clock */
+
+    RTC_WaitForSynchro(); /* Wait for RTC APB registers synchronisation */
+    RTC_WaitForLastTask(); /* Wait until last write operation on RTC registers has finished */
+
+    /* Set the RTC time base to 1s */
+    RTC_SetPrescaler(39999); /* RTC period = RTCCLK/RTC_PR = (40 KHz)/(39999+1) */
+    RTC_WaitForLastTask(); /* Wait until last write operation on RTC registers has finished *
+    
+    
+    
     RTC_ITConfig(RTC_IT_ALR,ENABLE); /* Enable the RTC Alarm */
     RTC_WaitForLastTask(); /* Wait until last write operation */
 }
@@ -86,4 +108,16 @@ void RTC_WaitForSynchro(void)
   RTC->CRL &= (uint16_t)~RTC_FLAG_RSF;
   /* Loop until RSF flag is set */
   while ((RTC->CRL & RTC_FLAG_RSF) == (uint16_t)RESET) { }
+}
+
+
+void EXTI_Configuration(void)
+{ 
+
+    /* Configure EXTI Line17(RTC Alarm) to generate an interrupt on rising edge */
+    EXTI_ClearITPendingBit(EXTI_Line17);
+    EXTI_InitStructure.EXTI_Line    = EXTI_Line17;
+    EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;
+    EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+    EXTI_Init(&EXTI_InitStructure);
 }
