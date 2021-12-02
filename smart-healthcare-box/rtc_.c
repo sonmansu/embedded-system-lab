5d0b1a0e3f11bb57747ab4c64bdbbdbb13b656ce
ambivalent46 <62247273+ambivalent46@users.noreply.github.com>
2021-12-02 12:56:03 +0900

    Update rtc_.c

--- smart-healthcare-box/rtc_.c
+++ smart-healthcare-box/rtc_.c
@@ -1,6 +1,10 @@
-// https://www.basic4mcu.com/bbs/board.php?bo_table=gesiyo4&wr_id=43&sca=STM32&page=2&device=pc
-// 노력하고 있습니다!
-include "stm32f10x_rtc.h"
+// http://poohyhoh.blogspot.com/2010/09/arm-cortex-m3-stm32-power-control-pwr-5.html
+// ...
+#include "stm32f10x_rtc.h"
+#define CRL_CNF_Set      ((uint16_t)0x0010)      /*!< Configuration Flag Enable Mask */
+#define CRL_CNF_Reset    ((uint16_t)0xFFEF)      /*!< Configuration Flag Disable Mask */
+#define PRLH_MSB_Mask    ((uint32_t)0x000F0000)  /*!< RTC Prescaler MSB Mask */
+#define RTC_LSB_Mask     ((uint32_t)0x0000FFFF)  /*!< RTC LSB Mask */
 
 
 void NVIC_Configuration(void) {
@@ -15,7 +19,7 @@ void NVIC_Configuration(void) {
 void RTC_IRQHandler(void) {
     if (RTC_GetITStatus(RTC_IT_SEC) != RESET) {
         RTC_ClearITPendingBit(RTC_IT_SEC); /* Clear the RTC Second interrupt */
-        LED_Toggle_Red(); /* Toggle Red */
+       // LED_Toggle_Red(); /* Toggle Red */
         TimeDisplay = 1; /* Enable time update */
 
         RTC_WaitForLastTask(); /* Wait until last write operation on RTC registers has finished */
@@ -32,4 +36,54 @@ void RTC_Configuration(void) {
     */
     RTC_ITConfig(RTC_IT_ALR,ENABLE); /* Enable the RTC Alarm */
     RTC_WaitForLastTask(); /* Wait until last write operation */
+}
+
+void RTC_ITConfig(uint16_t RTC_IT, FunctionalState NewState)
+{
+  assert_param(IS_RTC_IT(RTC_IT)); /* Check the parameters */
+  assert_param(IS_FUNCTIONAL_STATE(NewState));
+  
+  if (NewState != DISABLE) {
+    RTC->CRH |= RTC_IT;
+  } else {
+    RTC->CRH &= (uint16_t)~RTC_IT;
+  }
+}
+
+void RTC_EnterConfigMode(void)
+{
+  /* Set the CNF flag to enter in the Configuration Mode */
+  RTC->CRL |= CRL_CNF_Set;
+}
+
+void RTC_ExitConfigMode(void)
+{
+  /* Reset the CNF flag to exit from the Configuration Mode */
+  RTC->CRL &= CRL_CNF_Reset;
+}
+
+void RTC_SetPrescaler(uint32_t PrescalerValue)
+{
+  assert_param(IS_RTC_PRESCALER(PrescalerValue)); /* Check the parameters */
+  
+  RTC_EnterConfigMode();
+  /* Set RTC PRESCALER MSB word */
+  RTC->PRLH = (PrescalerValue & PRLH_MSB_Mask) >> 16;
+  /* Set RTC PRESCALER LSB word */
+  RTC->PRLL = (PrescalerValue & RTC_LSB_Mask);
+  RTC_ExitConfigMode();
+}
+
+void RTC_WaitForLastTask(void)
+{
+  /* Loop until RTOFF flag is set */
+  while ((RTC->CRL & RTC_FLAG_RTOFF) == (uint16_t)RESET) { }
+}
+
+void RTC_WaitForSynchro(void)
+{
+  /* Clear RSF flag */
+  RTC->CRL &= (uint16_t)~RTC_FLAG_RSF;
+  /* Loop until RSF flag is set */
+  while ((RTC->CRL & RTC_FLAG_RSF) == (uint16_t)RESET) { }
 }
